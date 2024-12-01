af35a115c20e9e2e66e1eae2a8ea0ea8922823af
tungetyt <michal.kotlicki.eu@gmail.com>
2024-12-01 18:00:28 +0100

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,28 @@ import Image from "next/image";
 import { Hero } from "@/components/hero";
 export default function Home() {
   return (
-      <main>
-        <Hero />
-      </main>
+    <main>
+      <Hero ><><div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+        <Image
+          className="invert dark:invert-0"
+          src="/vavicom-logo-1_white.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+      </div>
+        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+          Kompleksowa obsługa księgowa
+          <p className="text-center">+48 884 267 820</p>
+          <p className="text-center">info@vavicom.pl</p>
+        </div>
+        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+          KONTAKT
+        </button>
+      </>
+      </Hero>
+    </main>
   );
 }
+
--- components/hero.tsx
+++ components/hero.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import { AuroraBackground } from "./ui/aurora-background";
 import Image from "next/image";
 
-export function Hero() {
+export function Hero({ children }: { children: React.ReactNode }) {
     return (
         <AuroraBackground>
             <motion.div
@@ -18,24 +18,7 @@ export function Hero() {
                 }}
                 className="relative flex flex-col gap-4 items-center justify-center px-4"
             >
-                <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-                    <Image
-                        className="invert dark:invert-0"
-                        src="/vavicom-logo-1_white.png"
-                        alt="Next.js logo"
-                        width={180}
-                        height={38}
-                        priority
-                    />
-                </div>
-                <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-                    Kompleksowa obsługa księgowa
-                    <p className="text-center">+48 884 267 820</p>
-                    <p className="text-center">info@vavicom.pl</p>
-                </div>
-                <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-                    KONTAKT
-                </button>
+                {children}
             </motion.div>
         </AuroraBackground>
     );
