94976d820ad6587dde6af52d205a26c36ecaeb32
Tungetyt <static0sphere@gmail.com>
2024-12-18 14:29:23 +0100

    refactor page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,75 +14,78 @@ import {
 	IconTaxEuro
 } from '@tabler/icons-react'
 import Image from 'next/image'
+import type {ReactNode} from 'react'
 
-export default function Home() {
-	const products = [
-		<>
-			<IconBook2 key='IconBook2' size={64} />
-			Księgowość
-		</>,
-		<>
-			<IconCoins key='IconCoins' size={64} />
-			Kadry i płace
-		</>,
-		<>
-			<IconTaxEuro key='IconTaxEuro' size={64} />
-			Rozliczenia
-		</>,
-		<>
-			<IconReportMoney key='IconReportMoney' size={64} />
-			Usługi finansowe
-		</>,
-		<>
-			<IconReportAnalytics key='IconReportAnalytics' size={64} />
-			Usługi biznesowe
-		</>,
-		<>
-			<IconGavel key='IconGavel' size={64} />
-			Usługi pozostałe
-		</>
-	] as const
+const size = 64
 
-	return (
-		<>
-			<Hero>
-				<>
-					<div className='text-3xl md:text-7xl mt-32 sm:mt-0 font-bold dark:text-white text-center'>
-						<Image
-							className='invert'
-							src='/vavicom-logo-1_white.png'
-							alt='Next.js logo'
-							width={180}
-							height={38}
-							priority
-						/>
+const products = [
+	<>
+		<IconBook2 key='IconBook2' size={size} />
+		Księgowość
+	</>,
+	<>
+		<IconCoins key='IconCoins' size={size} />
+		Kadry i płace
+	</>,
+	<>
+		<IconTaxEuro key='IconTaxEuro' size={size} />
+		Rozliczenia
+	</>,
+	<>
+		<IconReportMoney key='IconReportMoney' size={size} />
+		Usługi finansowe
+	</>,
+	<>
+		<IconReportAnalytics key='IconReportAnalytics' size={size} />
+		Usługi biznesowe
+	</>,
+	<>
+		<IconGavel key='IconGavel' size={size} />
+		Usługi pozostałe
+	</>
+] as const satisfies Array<ReactNode>
+
+const Home = () => (
+	<>
+		<Hero>
+			<>
+				<div className='text-3xl md:text-7xl mt-32 sm:mt-0 font-bold dark:text-white text-center'>
+					<Image
+						className='invert'
+						src='/vavicom-logo-1_white.png'
+						alt='Next.js logo'
+						width={180}
+						height={38}
+						priority
+					/>
+				</div>
+				<div className='text-center font-extralight text-base dark:text-neutral-200 py-4'>
+					<div className='text-lg sm:text-2xl md:text-2xl'>
+						Kompleksowa obsługa księgowa
 					</div>
-					<div className='text-center font-extralight text-base dark:text-neutral-200 py-4'>
-						<div className='text-lg sm:text-2xl md:text-2xl'>
-							Kompleksowa obsługa księgowa
-						</div>
-						<div className='text-base sm:text-lg md:text-xl'>
-							<p>Oddziały: Warszawa • Józefosław</p>
-							<p className='flex items-center justify-center gap-1'>
-								<IconPhone />
-								{contactPhone}
-							</p>
-							<p className='flex items-center justify-center gap-1'>
-								<IconMail />
-								{contactEmail}
-							</p>
-						</div>
+					<div className='text-base sm:text-lg md:text-xl'>
+						<p>Oddziały: Warszawa • Józefosław</p>
+						<p className='flex items-center justify-center gap-1'>
+							<IconPhone />
+							{contactPhone}
+						</p>
+						<p className='flex items-center justify-center gap-1'>
+							<IconMail />
+							{contactEmail}
+						</p>
 					</div>
-					<FormModal />
-				</>
-			</Hero>
-			<div className='text-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:max-w-md md:max-w-2xl justify-items-center mx-auto [&>*]:[width:clamp(4rem,90%,16rem)] '>
-				{images.slice(0, products.length).map((image, index) => (
-					<ProductCard key={image} imageURL={image}>
-						{products[index]}
-					</ProductCard>
-				))}
-			</div>
-		</>
-	)
-}
+				</div>
+				<FormModal />
+			</>
+		</Hero>
+		<div className='text-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:max-w-md md:max-w-2xl justify-items-center mx-auto [&>*]:[width:clamp(4rem,90%,16rem)] '>
+			{images.slice(0, products.length).map((image, index) => (
+				<ProductCard key={image} imageURL={image}>
+					{products[index]}
+				</ProductCard>
+			))}
+		</div>
+	</>
+)
+
+export default Home
