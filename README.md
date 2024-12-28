b40bcf57937d1cb8e3bf7272f24e8b3c6917a19f
Tungetyt <static0sphere@gmail.com>
2024-12-28 13:39:41 +0100

    feat(Images formats): jpg -> avif

--- README.md
+++ README.md
@@ -16,4 +16,6 @@ Panel klienta section
 1. Unified code for both - depending on the env variable different jsons, icons and pictures are used
 2. Deploy Exzanco first for testing
 
-Fix Performance and Accessibility issues
+Fix Performance issues
+
+commitlint
--- lib/images.tsx
+++ lib/images.tsx
@@ -1,13 +1,13 @@
 export const images = [
-	'/88723845556176902.jpg',
-	'/1438935597575109622.jpg',
-	'/3554977752061304601.jpg',
-	'/6656126476403113043.jpg',
-	'/8806819558644939404.jpg',
-	'/12950576627901361101.jpg',
-	'/15133808056233984268.jpg',
-	'/17768666122035772285.jpg'
-] as const satisfies Array<`/${number}.jpg`>
+	'/88723845556176902.avif',
+	'/1438935597575109622.avif',
+	'/3554977752061304601.avif',
+	'/6656126476403113043.avif',
+	'/8806819558644939404.avif',
+	'/12950576627901361101.avif',
+	'/15133808056233984268.avif',
+	'/17768666122035772285.avif'
+] as const satisfies Array<`/${number}.avif`>
 
 export function shuffle(array: Array<string>) {
 	let currentIndex = array.length
