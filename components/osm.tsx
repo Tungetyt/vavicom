3d101df1009bee64091e48453b06723914f50af2
MK <48414404+Tungetyt@users.noreply.github.com>
2025-01-10 15:37:35 +0100

    feat(OSM): show more Warsaw by default

--- components/osm.tsx
+++ components/osm.tsx
@@ -1,32 +1,12 @@
 'use client'
 
-import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet'
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
 
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import 'leaflet-defaulticon-compatibility'
 import {locations} from '@/consts'
-import L from 'leaflet'
-import {useEffect} from 'react'
 import type {Writable} from 'type-fest'
-
-const FitBounds = ({
-	coordinates
-}: {
-	coordinates: Array<Writable<(typeof locations)[number]['coordinates']>>
-}) => {
-	const map = useMap()
-
-	useEffect(() => {
-		if (!map || coordinates.length === 0) return
-		map.fitBounds(L.latLngBounds(coordinates), {
-			padding: [100, 100],
-			maxZoom: 16
-		})
-	}, [map, coordinates])
-
-	return null
-}
 
 const OSM = () => {
 	return (
@@ -34,13 +14,15 @@ const OSM = () => {
 			style={{height: '100%', width: '100%'}}
 			attributionControl={false}
 			scrollWheelZoom={false}
+			zoom={11}
+			center={[
+				52.15,
+				locations
+					.map(({coordinates}) => coordinates[1])
+					.reduce((acc, lon) => acc + lon, 0) / locations.length
+			]}
 		>
 			<TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-			<FitBounds
-				coordinates={locations.map(
-					({coordinates}) => coordinates as Writable<typeof coordinates>
-				)}
-			/>
 			{locations.map(({address, coordinates}) => (
 				<Marker
 					key={address}
